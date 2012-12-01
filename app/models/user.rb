4e7a2727cbb26f0125055c96e6032352bce8fb35
samgooi4189 <samgooi4189@samgooi4189-VirtualBox.(none)>
2012-12-01 04:39:49 -0600

    added validation for user, including email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :question_done, :username
+  validates :email, :presence =>true, :email => true
+  validates :password, :presence =>true, :length => { :minimum => 8}
+  validates :username, :presence =>true, :length => { :maximum => 25}
 end
