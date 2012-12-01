c5cc62fc463d62c5b140e09a3cc5106a1d6d46e1
samgooi4189 <samgooi4189@samgooi4189-VirtualBox.(none)>
2012-12-01 04:07:04 -0600

    Clean environment for rails dev

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VeronaDev::Application.config.secret_token = 'c38e23c98c1a5843f37a3643ec39da9c44b813e00f714a40e468288aebaed57a577b937fb46180f451730963a4338d8f68e3cf4dcda7bde63ea0d6956c1f668d'
