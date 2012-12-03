b5097f312b4f1b5a70a4e0004431efc399f16eda
samgooi4189 <samgooi4189@samgooi4189-VirtualBox.(none)>
2012-12-02 20:28:59 -0600

    Remove username from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :email, :password, :question_done, :username
+  attr_accessible :email, :password, :question_done
   has_many :question
   validates :email, :presence =>true, :email => true
   validates_uniqueness_of :email
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121201230318) do
+ActiveRecord::Schema.define(:version => 20121203011904) do
 
   create_table "questions", :force => true do |t|
     t.string   "title"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(:version => 20121201230318) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "username"
     t.string   "email",                  :default => "", :null => false
     t.string   "password"
     t.integer  "question_done"
